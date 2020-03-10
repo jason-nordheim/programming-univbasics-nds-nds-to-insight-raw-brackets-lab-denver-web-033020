21d458935ed2ebc7d7cdf3af96e35d3bcda36bee
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-10 21:00:51 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -32,6 +32,7 @@ def directors_totals(nds)
     while inner_index < nds[:movies].count do
       gross_sales = nds[:movies][inner_index][:worldwide_gross]
       worldwide_gross += gross_sales
+      inner_index += 1 
     end
   end
 end
